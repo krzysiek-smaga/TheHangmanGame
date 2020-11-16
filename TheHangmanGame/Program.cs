4144d58f9dfef7aa8990df1c9ba0c939197ba8f3
Krzysiek Smaga <krzysiek.at@gmail.com>
2020-11-16 16:52:02 +0100

    Complete logical functionality of game

--- TheHangmanGame/Program.cs
+++ TheHangmanGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TheHangmanGame
 {
@@ -10,6 +11,189 @@ namespace TheHangmanGame
     {
         static void Main(string[] args)
         {
+            // Lists of EUROPEAN countries and capitals 
+            /*
+            List<string> europeanCountries = new List<string>();
+            List<string> europeanCapitals = new List<string>();
+            foreach (string line in File.ReadAllLines(@"C:\Users\krzys\source\repos\TheHangmanGame\TheHangmanGame\european_countries_and_capitals.txt"))
+            {
+                string[] words = line.Split('|');
+                europeanCountries.Add(words[0].Trim());
+                europeanCapitals.Add(words[1].Trim());
+            }
+            */
+
+            // Import capitals and countries from attached file
+            List<string> countries = new List<string>();
+            List<string> capitals = new List<string>();
+            foreach (string line in File.ReadAllLines(@"C:\Users\krzys\source\repos\TheHangmanGame\TheHangmanGame\countries_and_capitals.txt"))
+            {
+                string[] words = line.Split('|');
+                countries.Add(words[0].Trim());
+                capitals.Add(words[1].Trim());
+            }
+            
+            // Pick one random capital-country pair from list. Country for hint message.
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(capitals.Count);
+            string capitalToGuess = capitals[randomIndex].ToUpper();
+            string hint = $"The capital of {countries[randomIndex]}";
+
+
+            // static capital for tests
+            capitalToGuess = "WARSAW";
+
+            // Declaring some necessary variables
+            bool won = false;
+            int playerLifes = 7;
+            string title = "The Hangman Game";
+            List<string> notInWord = new List<string>();
+            //string playAgain = "";
+
+            
+            
+            string alphabet = "abcdefghijklmnoprqstuwvxyz";
+            alphabet = alphabet.ToUpper();
+
+            // Capital with
+            
+            
+            // Making visualization in dashes of capital tu guess
+            string[] dashes = new string[capitalToGuess.Length];
+            for (int i = 0; i < capitalToGuess.Length; i++)
+            {
+                switch (capitalToGuess[i])
+                {
+                    case '-':
+                        dashes[i] = "-";
+                        break;
+                    case ' ':
+                        dashes[i] = " ";
+                        break;
+                    default:
+                        dashes[i] = "_";
+                        break;
+                }
+            }
+            
+            
+
+
+
+
+            do
+            {
+                //Print title screen, capital in dashes to guess, lifes, hint if one life left
+                Console.Clear();
+                Console.WriteLine(title);
+                Console.WriteLine();
+                Console.WriteLine(" " + String.Join(" ", dashes));
+                Console.WriteLine();
+                Console.WriteLine($"Life: {playerLifes}");
+                if (playerLifes == 1)
+                {
+                    Console.WriteLine($"Hint: {hint}");
+                }
+                Console.WriteLine();
+
+                //Ask if he/she woould like to guess letter or whole word
+                //Do loop while he/she input correct letter
+                string choose = "";
+                do
+                {
+                    Console.WriteLine("Would You like to guess a [L]etter or whole [W]ord(s)?");
+                    Console.WriteLine("Type 'L' or 'W' and press Enter");
+                    string playerChoose = Console.ReadLine();
+                    choose = Convert.ToString(playerChoose).ToUpper();
+                } while (choose != "L" && choose != "W");
+
+               
+                if (choose == "L") // when player chose to type letter
+                {
+                    string playerGuessLetter = "";
+                    do
+                    {
+                        Console.WriteLine("Type a letter and press Enter");
+                        string playerInputLetter = Console.ReadLine();
+                        playerGuessLetter = Convert.ToString(playerInputLetter).ToUpper();
+                    } while (!(playerGuessLetter.Length == 1 && alphabet.Contains(playerGuessLetter)));
+
+                    if (capitalToGuess.Contains(playerGuessLetter))
+                    {
+                        //exchange letters in dashes
+                        for (int i = 0; i < capitalToGuess.Length; i++)
+                        {
+                            if (playerGuessLetter[0] == capitalToGuess[i])
+                            {
+                                dashes[i] = playerGuessLetter;
+                            }
+                        }
+                        //compare dashes with capital to guess
+                        if (String.Join("", dashes) == capitalToGuess)
+                        {
+                            won = true;
+                        }
+                    }
+                    else
+                    {
+                        //add letter to not-in-word list
+                        notInWord.Add(playerGuessLetter);
+                        //take one life
+                        playerLifes--;
+                    }
+                }
+                else // when player chose to type whole word(s)
+                {
+                    Console.WriteLine("Type a word(s) and press Enter");
+                    string playerInputWord = Console.ReadLine();
+                    string playerGuessWord = Convert.ToString(playerInputWord).ToUpper();
+
+                    if (playerGuessWord == capitalToGuess)
+                    {
+                        // if player guessed capital correct then change won to true
+                        won = true;
+                    }
+                    else
+                    {
+                        // take two lifes
+                        playerLifes -= 2;
+                    }
+                }
+
+
+                Console.ReadLine();
+            } while (!won && playerLifes > 0);
+
+            Console.Clear();
+            Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine(" " + String.Join(" ", dashes));
+            Console.WriteLine();
+
+            if (won)
+            {
+                Console.WriteLine("Congrats! You Won!");
+            }
+            else
+            {
+                Console.WriteLine("You lost! Better luck next time!");
+            }
+
+
+            /*
+            string choosePlay = "";
+            Console.WriteLine("Would You like to play again?");
+            Console.WriteLine("Type 'Y' to try again or press any key to quit.");
+            string playerChoose2 = Console.ReadLine();
+            choosePlay = Convert.ToString(playerChoose2).ToUpper();
+            */
+
+
+
+            Console.ReadLine();
+
+            Console.WriteLine("End of Program");
+            Console.ReadLine();
         }
     }
 }
